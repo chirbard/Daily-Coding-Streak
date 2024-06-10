70572218da913d269e18c9dd248b1d7a5d4dd783
chirbard <73120520+chirbard@users.noreply.github.com>
2024-06-10 12:15:26 +0000

    Daily commit 2024-06-10

---
+++ 2024-06-10/main.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        # Using breatdh first search.
+        queue = [root]
+        lst = []
+        while queue:
+            current_node = queue.pop(0)
+            lst.append(current_node.val)
+
+            if current_node.left:
+                queue.append(current_node.left)
+            if current_node.right:
+                queue.append(current_node.right)
+
+        lst.sort()
+        return lst[k - 1]
+        
